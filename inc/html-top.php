666e334f085c38f62ffb81e8f6c48f67a554c4a6
rakomea <55210364+rakomea@users.noreply.github.com>
2019-11-20 12:40:08 -0500

    Add files via upload

---
+++ inc/html-top.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+	<meta charset="utf-8">
+	<title>Assignment 6 - Brochure Website</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/override.css">
+</head>
---
+++ inc/nav.php
@@ -0,0 +1,11 @@
+<nav class="menu">
+	<div class="nav-wrapper">
+	<a href="#" class="brand-logo left logo">OBOC</a>
+	<ul class="right">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="members.php">Members</a></li>
+		<li><a href="productions.php">Past Productions</a></li>
+		<li><a href="form.php">Contact Us</a></li>
+	</ul>
+</div>
+</nav>
